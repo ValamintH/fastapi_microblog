f60ac17fb0416fb33087c91bd7591cf69a0b47a5
Valamint <kuzminvm@inbox.ru>
2023-10-05 18:45:01 +0300

    feat: Добавлено логирование в файл и отправка ошибок на почту

--- app/config.py
+++ app/config.py
@@ -1,6 +1,18 @@
+import os
+
 from fastapi.templating import Jinja2Templates
 
 SECRET_KEY = "secret-key"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 templates = Jinja2Templates(directory="templates")
+
+
+MAIL_CONFIG_DICT = {
+    "MAIL_SERVER": os.environ.get("MAIL_SERVER"),
+    "MAIL_PORT": int(os.environ.get("MAIL_PORT") or 25),
+    "MAIL_USE_TLS": os.environ.get("MAIL_USE_TLS") is not None,
+    "MAIL_USERNAME": os.environ.get("MAIL_USERNAME"),
+    "MAIL_PASSWORD": os.environ.get("MAIL_PASSWORD"),
+    "ADMINS": ["your-email@example.com"],
+}
--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler, SMTPHandler
+
 import uvicorn
-from config import SECRET_KEY, templates
+from config import MAIL_CONFIG_DICT, SECRET_KEY, templates
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from routers.auth import router as auth_router
@@ -17,6 +21,36 @@ app.mount(
 )
 app.include_router(auth_router)
 app.include_router(front_router)
+
+
+@app.on_event("startup")
+async def startup_event():
+    if MAIL_CONFIG_DICT["MAIL_SERVER"]:
+        auth = None
+        if MAIL_CONFIG_DICT["MAIL_USERNAME"] or MAIL_CONFIG_DICT["MAIL_PASSWORD"]:
+            auth = (MAIL_CONFIG_DICT["MAIL_USERNAME"], MAIL_CONFIG_DICT["MAIL_PASSWORD"])
+        secure = None
+        if MAIL_CONFIG_DICT["MAIL_USE_TLS"]:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(MAIL_CONFIG_DICT["MAIL_SERVER"], MAIL_CONFIG_DICT["MAIL_PORT"]),
+            fromaddr="no-reply@" + MAIL_CONFIG_DICT["MAIL_SERVER"],
+            toaddrs=MAIL_CONFIG_DICT["ADMINS"],
+            subject="Microblog Failure",
+            credentials=auth,
+            secure=secure,
+        )
+        mail_handler.setLevel(logging.ERROR)
+        logging.getLogger("uvicorn.error").addHandler(mail_handler)
+
+    if not os.path.exists("logs"):
+        os.mkdir("logs")
+    file_handler = RotatingFileHandler("logs/microblog.log", maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(
+        logging.Formatter("%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]")
+    )
+    file_handler.setLevel(logging.INFO)
+    logging.getLogger("uvicorn.access").addHandler(file_handler)
 
 
 @app.exception_handler(404)
