f988878d01a72880a09bd8f3c2964ab735dc113f
Valamint <kuzminvm@inbox.ru>
2023-10-08 21:40:00 +0300

    chore: Конфигурация мейла теперь просто класс

--- app/config.py
+++ app/config.py
@@ -1,7 +1,6 @@
 import os
 
 from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel
 
 SECRET_KEY = "secret-key"
 ALGORITHM = "HS256"
@@ -9,7 +8,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 templates = Jinja2Templates(directory="templates")
 
 
-class MailConfig(BaseModel):
+class MailConfig(object):
     MAIL_SERVER = os.environ.get("MAIL_SERVER")
     MAIL_PORT = int(os.environ.get("MAIL_PORT") or 25)
     MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
