37ba01d8aca58e5ae54ff7ca5c6e9a9dc96f03b4
Valamint <kuzminvm@inbox.ru>
2023-10-25 11:16:10 +0300

    fix: Удален лишний хэндлер

--- app/set_uvicorn_handlers.py
+++ app/set_uvicorn_handlers.py
@@ -1,26 +1,6 @@
 import logging
 import os
-from logging.handlers import RotatingFileHandler, SMTPHandler
-
-
-def set_mail_handler(mail_config):
-    if mail_config.MAIL_SERVER:
-        auth = None
-        if mail_config.MAIL_USERNAME or mail_config.MAIL_PASSWORD:
-            auth = (mail_config.MAIL_USERNAME, mail_config.MAIL_PASSWORD)
-        secure = None
-        if mail_config.MAIL_USE_TLS:
-            secure = ()
-        mail_handler = SMTPHandler(
-            mailhost=(mail_config.MAIL_SERVER, mail_config.MAIL_PORT),
-            fromaddr="no-reply@" + mail_config.MAIL_SERVER,
-            toaddrs=mail_config.ADMINS,
-            subject="Microblog Failure",
-            credentials=auth,
-            secure=secure,
-        )
-        mail_handler.setLevel(logging.ERROR)
-        logging.getLogger("uvicorn.error").addHandler(mail_handler)
+from logging.handlers import RotatingFileHandler
 
 
 def set_file_handler():
